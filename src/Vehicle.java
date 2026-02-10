eaad924b1dfbfb22df3cbae65aa29234b15249d0
Akila Deshan <akiladeshanprosl@gmail.com>
2026-02-11 01:59:00 +0530

    Completed the Vehicle.java file

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,52 @@
-public class Vehicle {
-    public static void main(String[] args){
-        System.out.println("Hello");
+public abstract class Vehicle {
+    private final String vehicleId;
+    private final String brand;
+    private final String model;
+    private final double baseRatePerDay;
+    private boolean isAvailable;
+
+    public Vehicle(String vehicleId, String brand, String model, double baseRatePerDay){
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.baseRatePerDay = baseRatePerDay;
+        this.isAvailable = true;
+    }
+
+    public String getVehicleId(){
+        return vehicleId;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getModel(){
+        return model;
+    }
+    public double getBaseRatePerDay(){
+        return baseRatePerDay;
+    }
+    public boolean getIsAvailable(){
+        return isAvailable;
+    }
+
+    public abstract double calculateRentalCost(int days);
+
+    public void rentVehicle(){
+        if (isAvailable){
+            isAvailable = false;
+            System.out.println("Vehicle " + vehicleId + " has been rented.");
+        }else{
+            System.out.println("The vehicle is already rented");
+        }
+    }
+
+    public void returnVehicle(){
+        isAvailable = true;
+        System.out.println("Vehicle " + vehicleId + " has been returned.");
+    }
+
+    public void displayDetails(){
+        String status = isAvailable ? "Available" : "Rented";
+        System.out.println("Id: " + vehicleId + " | " + brand + " " + model + " | Rate: Rs: " + baseRatePerDay + " day | Status: " + status);
     }
 }
