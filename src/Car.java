6e2a590b74d49a9c1b2cd89e609edb35abbc5219
dhanushka-anuruddha <dhanush2002420@gmail.com>
2026-02-11 02:50:48 +0530

    completed the bike.java, car.java, van.java files

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,20 @@
-public class Car {
+public class Car extends Vehicle {
+    private int numberOfSeats;
+
+    public Car(String vehicleId, String brand, String model, double baseRatePerDay, int numberOfSeats) {
+        super(vehicleId, brand, model, baseRatePerDay);
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return (getBaseRatePerDay() * days) + (numberOfSeats * 200 * days);
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("   Type: Car | Seats: " + numberOfSeats);
+        System.out.println("------------------------------------------------");
+    }
 }
--- src/Van.java
+++ src/Van.java
@@ -1,2 +1,20 @@
-public class Van {
+public class Van extends Vehicle {
+    private double cargoCapacityKg;
+
+    public Van(String vehicleId, String brand, String model, double baseRatePerDay, double cargoCapacityKg) {
+        super(vehicleId, brand, model, baseRatePerDay);
+        this.cargoCapacityKg = cargoCapacityKg;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return (getBaseRatePerDay() * days) + (cargoCapacityKg * 0.2 * days);
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("   Type: Van | Cargo: " + cargoCapacityKg + "Kg");
+        System.out.println("------------------------------------------------");
+    }
 }
